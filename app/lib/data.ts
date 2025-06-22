86585a87d5c01cf4483a5dfbe63250951bb1048d
Andhika Dwitama <andhikaDWA@gmail.com>
2025-06-22 09:36:06 +0700

    Add pagination and search features (Chapter 11)

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -156,7 +156,7 @@ export async function fetchInvoiceById(id: string) {
     const invoice = data.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
-      amount: invoice.amount / 100,
+      amount: (invoice.amount / 100) * 16530,
     }));
 
     return invoice[0];
